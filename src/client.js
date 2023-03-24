7e718a9643d1eaec038fd20042233fdaf0353189
ihlmpy <jsih1236@gmail.com>
2023-03-24 17:08:07 +0100

    veh command

--- src/client.js
+++ src/client.js
@@ -28,9 +28,47 @@ RegisterCommand("fraction", async (src, args, rawCommand) => {
                 false,
             );
        });
+
+       SetEntityHealth(ped, 200);
+       SetPedArmour(ped, 200);
    } else {
        console.log("GANG");
    }
+});
 
-   console.log({ ped, player, x, y, z });
+RegisterCommand("xyz", async (src) => {
+    const ped = GetPlayerPed(src);
+    console.log({ ped, xyz: GetEntityCoords(ped) });
+})
+
+RegisterCommand("getPoliceVeh", async (src, arg) => {
+    const pedID = GetPlayerPed(src);
+    const [x, y, z] = GetEntityCoords(pedID, true);
+
+    if (arg[0]) { 
+        RequestModel(arg[0]);
+        const vehicleID = CreateVehicle(arg[0], x, y, z, GetEntityHeading(pedID), true, true);
+        SetEntityCoords(vehicleID, x, y, z, false, false, false, false);
+        SetPedIntoVehicle(pedID, vehicleID, -1);
+    } else {
+        emit("chat:addMessage", { 
+            color: [255, 0, 0], 
+            multiline: true, 
+            args: [
+                "Me",
+                "policeold2",
+                "sheriff - Ford Crown Victoria LSSD", 
+                "sheriff2 - Ford Explorer LSSD", 
+                "policeb - Police Bike", 
+                "polmav - Police Maverick", 
+                "schafter5 - Detective Cruiser"
+            ] 
+        });
+    }
+    
+    /*RequestModel("police3");
+    const vehicleID = CreateVehicle("police3", x, y, z, GetEntityHeading(pedID), true, true);
+    SetEntityCoords(vehicleID, x, y, z, false, false, false, false);
+    SetPedIntoVehicle(100, vehicleId, -1);
+    console.log(IsModelInCdimage("pony"));*/
 });
